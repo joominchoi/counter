db0b8170f6354fbefbe4e5b7276e229883928d96
JooMin Choi <joominchoi23@gmail.com>
2023-09-09 13:47:42 +0900

    Change color of number according to value

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ buttons.forEach(function (button) {
     } else {
       count = 0;
     }
+    if (count > 0) {
+      value.style.color = "green"
+    }
+    if (count < 0) {
+      value.style.color = "red"
+    }
+    if (count === 0) {
+      value.style.color = "#222"
+    } 
 
     value.textContent = count;
   })
